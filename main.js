80443c81690bc59b08d7e2a775cad5b0bc1a56fc
oguztecirlioglu <69775148+oguztecirlioglu@users.noreply.github.com>
2020-09-27 12:02:05 +0100

    Updated again

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-particlesJS.load('particles-js', 'assets/particles.json', function() {
-    console.log('callback - particles.js config loaded');
-  });   
+window.addEventListener("scroll", () => {
+    let content = document.querySelectorAll(".scroll-anim");
+    for (let i = 0; i < content.length; i++) {
+         let contentPosition = content[i].getBoundingClientRect().top;
+         let screenPosition = window.innerHeight;
+        if (contentPosition * 1.25 < screenPosition) {
+            content[i].classList.add('scroll-anim-active')
+        }
+        else {
+            content[i].classList.remove('scroll-anim-active')
+        }
+    }
+});
